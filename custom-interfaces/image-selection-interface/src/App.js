cb17767962ca20543dd555f195dabd301a969075
rramswamy <rramswamy@sighthound.com>
2021-05-11 10:34:43 -0400

    update

--- custom-interfaces/image-selection-interface/src/App.js
+++ custom-interfaces/image-selection-interface/src/App.js
@@ -38,7 +38,7 @@ const TopWrapper = styled.div`
 
 const ImagesWrapper = styled.div`
   padding: 5px;
-  width: 200px;
+  width: 50%;
   border-top: ${props => props.hasReferenceImage ? '1px solid lightgray' : 'none'};
 
   div.image-wrapper {
