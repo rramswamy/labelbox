66c78ed98fe191b6b32c9ae2fddacd0ca1900b3b
rramswamy <rramswamy@sighthound.com>
2021-05-19 15:38:58 -0400

    Update

--- custom-interfaces/image-selection-interface/src/App.js
+++ custom-interfaces/image-selection-interface/src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useState, useEffect } from 'react';
+//import { useState, useEffect } from 'react';
 import { LinearProgress } from "@material-ui/core";
 import styled from "styled-components";
 
